66144f406220b39868f2cddc09c14e26da85f67e
ZiyandaPetela <zandapetela@gmail.com>
2025-04-27 09:39:57 +0200

    created interfaces with an in-memory HashMap storage

--- src/main/java/repositories/StudentRepository.java
+++ src/main/java/repositories/StudentRepository.java
@@ -3,7 +3,7 @@ package repositories;
 import core.Student;
 import java.util.List;
 
-public interface studentRep extends Repository<Student, String> {
+public interface StudentRepository extends Repository<Student, String> {
     // Student-specific methods
     List<Student> findByAcademicYear(String academicYear);
     List<Student> findByMajor(String major);
---
+++ src/main/java/repositories/inmemory/InMemoryAppointmentRepository.java
@@ -0,0 +1,5 @@
+package repositories.inmemory;
+
+public class InMemoryAppointmentRepository {
+    
+}
